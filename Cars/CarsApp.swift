c2540a627c70a71c52f8e37c0c20ed525f157cca
rustamsafarovrs <rs.programmer@hotmail.com>
2022-09-19 11:28:52 +0500

    Initial Commit

---
+++ Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by Rustam Safarov on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
